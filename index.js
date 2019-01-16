15dc84b5c2ab690d4dfe70d1840d7e2093ac74a6
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-01-16 13:45:17 -0800

    Add/initialize  server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+// conect to the database
+const db = knex(knexConfig.development);
+
+//endpoints
+server.get('/', (req, res) => {
+	res.send('api working');
+});
+
+
+
+
+
+const port = 3300;
+server.listen(port, function() {
+	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
